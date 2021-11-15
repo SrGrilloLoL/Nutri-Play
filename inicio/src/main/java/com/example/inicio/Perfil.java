a50d3d4316960bc191d92f0fd7d3cfbbeb99a4b5
SrGilloLoL <alvaro18290927@itcg.edu.mx>
2021-11-15 13:51:08 -0600

    Segundo intento

--- inicio/src/main/java/com/example/inicio/Perfil.java
+++ inicio/src/main/java/com/example/inicio/Perfil.java
@@ -5,9 +5,11 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.unity3d.player.UnityPlayerActivity;
@@ -18,7 +20,7 @@ public class Perfil extends AppCompatActivity {
     private LinearLayout lvPerfil, lvActFisica, lvJuego, lvMenu;
     private TextView tvUsuario, tvGenero;
     private Button btPerfil, btActFisica, btJuego, btEditar, btGuardar, btCerrarSesion;
-    private EditText etEdad, etEstatura, etPeso;
+    private Spinner spEdadP, spEstaturaP, spPesoP;
     private String link = "https://www.youtube.com/channel/UCERTeaGoYINlLt9Y31_fmUw";
 
     @Override
@@ -41,9 +43,59 @@ public class Perfil extends AppCompatActivity {
         btGuardar = (Button) findViewById(R.id.btGuardar);
         btCerrarSesion = (Button) findViewById(R.id.btCerrarSesion);
 
-        etEdad = (EditText) findViewById(R.id.etEdad);
-        etEstatura = (EditText) findViewById(R.id.etEstatura);
-        etPeso = (EditText) findViewById(R.id.etPeso);
+        spEdadP = (Spinner) findViewById(R.id.spEdadP);
+        String edad[] = {"Selecciona", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"};
+        ArrayAdapter<String> adapterEdad = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item,
+                edad);
+        spEdadP.setAdapter(adapterEdad);
+        spEdadP.setEnabled(false);
+
+        spPesoP = (Spinner) findViewById(R.id.spPesoP);
+        String peso[] = {"Selecciona", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+                "10", "11", "12", "13", "14", "15", "16", "17", "18", "19",
+                "20", "21", "22", "23", "24", "25", "26", "27", "28", "29",
+                "30", "31", "32", "33", "34", "35", "36", "37", "38", "39",
+                "40", "41", "42", "43", "44", "45", "46", "47", "48", "49",
+                "50", "51", "52", "53", "54", "55", "56", "57", "58", "59",
+                "60", "61", "62", "63", "64", "65", "66", "67", "68", "69",
+                "70", "71", "72", "73", "74", "75", "76", "77", "78", "79",
+                "80", "81", "82", "83", "84", "85", "86", "87", "88", "89",
+                "90", "91", "92", "93", "94", "95", "96", "97", "98", "99",
+                "100"};
+        ArrayAdapter<String> adapterPeso = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item,
+                peso);
+        spPesoP.setAdapter(adapterPeso);
+        spPesoP.setEnabled(false);
+
+        spEstaturaP = (Spinner) findViewById(R.id.spEstaturaP);
+        String estatura[] = {"Selecciona", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+                "10", "11", "12", "13", "14", "15", "16", "17", "18", "19",
+                "20", "21", "22", "23", "24", "25", "26", "27", "28", "29",
+                "30", "31", "32", "33", "34", "35", "36", "37", "38", "39",
+                "40", "41", "42", "43", "44", "45", "46", "47", "48", "49",
+                "50", "51", "52", "53", "54", "55", "56", "57", "58", "59",
+                "60", "61", "62", "63", "64", "65", "66", "67", "68", "69",
+                "70", "71", "72", "73", "74", "75", "76", "77", "78", "79",
+                "80", "81", "82", "83", "84", "85", "86", "87", "88", "89",
+                "90", "91", "92", "93", "94", "95", "96", "97", "98", "99",
+                "100", "101", "102", "103", "104", "105", "106", "107", "108", "109",
+                "110", "111", "112", "113", "114", "115", "116", "117", "118", "119",
+                "120", "121", "122", "123", "124", "125", "126", "127", "128", "129",
+                "130", "131", "132", "133", "134", "135", "136", "137", "138", "139",
+                "140", "141", "142", "143", "144", "145", "146", "147", "148", "149",
+                "150", "151", "152", "153", "154", "155", "156", "157", "158", "159",
+                "160", "161", "162", "163", "164", "165", "166", "167", "168", "169",
+                "170", "171", "172", "173", "174", "175", "176", "177", "178", "179",
+                "180", "181", "182", "183", "184", "185", "186", "187", "188", "189",
+                "190", "191", "192", "193", "194", "195", "196", "197", "198", "199",
+                "200"};
+        ArrayAdapter<String> adapterEstatura = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item,
+                estatura);
+        spEstaturaP.setAdapter(adapterEstatura);
+        spEstaturaP.setEnabled(false);
 
     }
 
@@ -78,9 +130,9 @@ public class Perfil extends AppCompatActivity {
     }
 
     public void editarPerfil(View view){
-        etPeso.setEnabled(true);
-        etEstatura.setEnabled(true);
-        etEdad.setEnabled(true);
+        spPesoP.setEnabled(true);
+        spEstaturaP.setEnabled(true);
+        spEdadP.setEnabled(true);
 
 
 
@@ -89,9 +141,9 @@ public class Perfil extends AppCompatActivity {
     }
 
     public void guardarPerfil(View view){
-        etPeso.setEnabled(false);
-        etEstatura.setEnabled(false);
-        etEdad.setEnabled(false);
+        spPesoP.setEnabled(false);
+        spEstaturaP.setEnabled(false);
+        spEdadP.setEnabled(false);
 
 
 
