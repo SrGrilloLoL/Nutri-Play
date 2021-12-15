9b0488f3f4aa151d9f6dd24d06382636edfd3bb6
unknown <boiso.ska8@gmail.com>
2021-12-14 20:50:17 -0600

    La app ya apunta al vps de digital ocean

--- inicio/src/main/java/com/example/inicio/api/Uri.java
+++ inicio/src/main/java/com/example/inicio/api/Uri.java
@@ -2,6 +2,6 @@ package com.example.inicio.api;
 
 public class Uri {
 
-    public static String base = "https://3834-2806-102e-19-5e3f-6cfb-17b2-bcd7-e43f.ngrok.io/";
+    public static String base = "http://143.198.224.168/";
 
 }
